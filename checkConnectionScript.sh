7d6f80425f7f3592a2d733bfb0ab3399c5358bf4
pietrobalatti <pietrobalatti@gmail.com>
2016-08-03 21:49:48 +0200

    Added header

--- checkConnectionScript.sh
+++ checkConnectionScript.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+
+
+# title:		checkConnectionScript.sh
+# description:	This script reboots the system if no connection is available
+# author:		Pietro Balatti
+# date:			2016-08-03
+
 x=`ping -c1 google.com 2>&1 | grep unknown`
 if [ ! "$x" = "" ]; then
     echo "Restarted on date: $(date)" >> ./path/to/file/log.txt
